40bdc7b57eaa7159a3accaaa66b94562c77d2e07
evonet2015 <common.evonet@gmail.com>
2017-11-16 14:27:36 +0300

    Update sessions-sender.sh

--- resources/sessions-sender.sh
+++ resources/sessions-sender.sh
@@ -1,2 +1,2 @@
 VAR=$(iw dev wlan0 station dump);
-wget http://app.evonet.by/device/sessions/70/ -O /dev/null --post-data="$VAR"
+wget http://app.evonet.by/device/sessions/15/ -O /dev/null --post-data="$VAR"
